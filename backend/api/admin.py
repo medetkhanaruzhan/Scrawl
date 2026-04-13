28db167d5b797ea3bb8f49c58a8c1cb5d71324a1
aruzhan <medetkhan.aruzhan@gmail.com>
2026-04-13 17:32:36 +0500

    Delete backend directory

--- backend/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Perfume, PreferenceProfile, Recommendation
-
-admin.site.register(Profile)
-admin.site.register(Perfume)
-admin.site.register(PreferenceProfile)
-admin.site.register(Recommendation)
